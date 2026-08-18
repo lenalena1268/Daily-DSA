e7773928897502435506be815d27ed5858319f5b
Lena Fathima <lenafathima2004@gmail.com>
2026-08-18 11:10:03 +0530

    Time: 3 ms (12.42%), Space: 19.2 MB (58.96%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+       total = 0
+       for num in  nums:
+           if nums.count(num) == 1:
+            total += num
+       return total
+             
+
+        
+       
